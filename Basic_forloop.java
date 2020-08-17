ee1e03dc1b3ba9946f382b7fab09d0e5f846ce57
Kantinun <s6201012630011@email.kmutnb.ac.th>
2020-08-17 18:41:21 +0700

    Basic for loop

---
+++ Basic_forloop.java
@@ -0,0 +1,8 @@
+class Basic_forloop{
+	public static void main(String[] args){
+		int round = Integer.parseInt(args[0]);
+		for(int i = 0; i< round; i++){
+			System.out.println("Count"+i);
+		}
+	}
+}
