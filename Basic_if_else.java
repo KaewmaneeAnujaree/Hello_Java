900bbe8d8115fbee4d62503e3674d37aa60514b7
Kantinun <s6201012630011@email.kmutnb.ac.th>
2020-08-17 18:11:16 +0700

    Basic_if_else

---
+++ Basic_if_else.java
@@ -0,0 +1,10 @@
+class Basic_if_else{
+	public static void main(String[] args){
+		int input = Integer.parseInt(args[0]);
+		if(input < 1){
+			System.out.println("1 is great than input");
+		}else{
+			System.out.println("input great than 1");
+		}
+	}
+}
