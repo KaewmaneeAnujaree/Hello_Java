5465c56f77525fb65ee044c1388df338542e7ffa
Kaewmanee Anujaree <s6201012620031@email.kmutnb.ac.th>
2020-08-18 12:59:26 +0700

    boolean

---
+++ Basic_boolean.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Basic_boolean{
+	public static void main(String[] args){
+    System.out.print("Please enter a Boolean value: ");
+    Scanner input = new Scanner(System.in);
+    boolean a = input.nextBoolean();
+    System.out.println(a);
+	}
+}
