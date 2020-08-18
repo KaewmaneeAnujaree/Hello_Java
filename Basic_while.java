1bdcc9ef905af354ebfab1ca815a9c4f11bbdd03
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2020-08-18 12:06:23 +0700

    Basic while

---
+++ Basic_while.java
@@ -0,0 +1,9 @@
+class Basic_while {
+    public static void main(String[] args){
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
