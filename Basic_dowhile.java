5e82b1e1f12d492f85ae532b5f36db185e35c7e2
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2020-08-18 12:23:36 +0700

    Basic_dowhile

---
+++ Basic_dowhile.java
@@ -0,0 +1,9 @@
+class Basic_dowhile {
+    public static void main(String[] args){
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 11);
+    }
+}
